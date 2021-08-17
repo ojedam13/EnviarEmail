01aa3c3cc8787ec9f3ae3e72964f5c3efa703827
ojedam13 <m_ojeda17@hotmail.com>
2021-08-17 11:33:46 -0300

    validar campos

--- js/app.js
+++ js/app.js
@@ -2,9 +2,22 @@
 const btnEnviar = document.querySelector('#enviar');
 
 
+//variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+
 eventListeners();
 function eventListeners() {
+    //cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    //campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
 }
 
 
@@ -14,4 +27,15 @@ function eventListeners() {
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+//validar el formulario
+
+function validarFormulario(e) {
+    if (e.target.value.length > 0) {
+        
+    } else {
+       
+        e.target.classList.add('border', 'border-red-500');
+    }
 }
