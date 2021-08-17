a59555fe2232b6b0facb8d9955caecfc81b75964
ojedam13 <m_ojeda17@hotmail.com>
2021-08-17 11:12:39 -0300

    cambios en boton enviar

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//variables
+const btnEnviar = document.querySelector('#enviar');
+
+
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+}
+
+
+
+//funciones
+
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
