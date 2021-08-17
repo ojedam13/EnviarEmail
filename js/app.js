fb9eba8477dc6d8c1be0a14ff361c0dd32cf8e82
ojedam13 <m_ojeda17@hotmail.com>
2021-08-17 17:17:23 -0300

    limpiar msj de error

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 
 //variables para campos
@@ -35,7 +36,23 @@ function validarFormulario(e) {
     if (e.target.value.length > 0) {
         
     } else {
-       
         e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
     }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+
+    if (errores.length === 0) {
+         formulario.appendChild(mensajeError);
+    }
+
+   
+
 }
